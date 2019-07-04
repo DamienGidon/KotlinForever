bc98d8d40c221fe7128327fd97b5d1e61eb26365
ThibaultCam <thibault.cam1@yahoo.fr>
2019-07-04 09:45:46 +0200

    1st commmmmit

---
+++ app/src/main/java/org/esiea/cam_gidon/kotlineval/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.esiea.cam_gidon.kotlineval
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
